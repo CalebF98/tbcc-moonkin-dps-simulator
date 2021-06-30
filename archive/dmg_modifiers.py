95701eb03d8f8cfea97e9c5bddb5c7fc5a54ad11
Caleb <cfringer98@gmail.com>
2021-06-30 11:20:29 -0700

    Extracted starfire sim into simulation.py.

---
+++ archive/dmg_modifiers.py
@@ -0,0 +1,53 @@
+dmg_modifiers = {
+	'buffs': {
+
+	},
+	'debuffs': {
+
+	},
+	'set_bonuses': {
+		'Spellfire',
+		'Spellstrike'
+	},
+	'talents': {
+		'Balance of Power': { 
+			'value': 4, 
+			'description': '+4% chance to hit w/ spells' 
+		},
+		'Focused Starlight': {
+			'value': 4,
+			'description': '+4% chance to crit w/ Starfire and Wrath'
+		},
+		'Moonkin Form': {
+			'value': 5,
+			'description': '+5% chance to hit w/ all spells'
+		},
+		'Improved Moonfire': {
+			'value': 10,
+			'description': '+10% damage and crit when casting Moonfire'
+		},
+		'Starlight Wrath': {
+			'value': True,
+			'description': '0.5 second cast time reduction for Starfire and Wrath'
+		},
+		'Vengeance': {
+			'value': True,
+			'description': 'Increases the critical strike damage bonus of Starfire, Moonfire, and Wrath spells by 100%'
+		},
+		'Lunar Guidance': {
+			'value': True,
+			'description': 'Gain 25% of intellectlect as spell damage'
+		},
+		'Moonfury': {
+			'value': 1.1,
+			'description': 'Increases the damage done by Starfire, Moonfire, and Wrath spells by 10%'
+		},
+		'Wrath of Cenarius': {
+			'value': { 'Starfire': 1.2, 'Wrath': 1.1},
+			'description': 'Starfire gains an additional 20% and Wrath gains an additional 10% of your bonus damage'
+		}
+	},
+	'trinkets': {
+
+	}
+}
