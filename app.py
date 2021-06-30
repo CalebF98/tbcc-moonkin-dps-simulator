11998c5945008c5c280658c513dd6ca4533cdc24
Caleb <cfringer98@gmail.com>
2021-06-30 12:20:08 -0700

    Implemented a basic Flask instance to run sim. Also added dependencies list in requirements.txt

--- app.py
+++ app.py
@@ -1,53 +1,8 @@
-from tkinter import *
-from tkinter import ttk
+from simulation import compute_avg_dps
+from flask import Flask
+app = Flask(__name__)
 
-class App(Tk):
-	def __init__(self):
-		super().__init__()
-		self.title('Moonkin DPS Simulator')
-		
-		# Init the main content frame 
-		self.content = ttk.Frame(self, padding="3 3 12 12")
-		self.content.grid(column=0, row=0, sticky=(N, W, E, S))
-
-		# Setup character stat variables for entry widgets
-		self.stats = {
-			'Intellect'   			: StringVar(name='int', 			 value=115),
-			'Spellpower'  			: StringVar(name='sp',         value=0),
-			'Spell Hit Rating'  : StringVar(name='spell_hit',  value=0),
-			'Spell Crit Rating'	: StringVar(name='spell_crit', value=0),
-			'Spell Haste' 			: StringVar(name='haste', 		 value=0)
-		}
-		
-		# Build stat input widgets & labels
-		self.create_widgets()
-
-	def create_widgets(self):
-		# Create a row w/ an entry and label for each player stat
-		for idx, (stat_name, tkVar) in enumerate(self.stats.items()):
-			entry = ttk.Entry(self.content, width=7, textvariable=tkVar)
-			label = ttk.Label(self.content, text=stat_name)
-
-			entry.grid(column=0, row=idx)
-			label.grid(column=1, row=idx)
-
-		# Row 5 / Calculate Button
-		submit_button = ttk.Button(self.content, command=self.calculate, text='Calculate DPS')
-		submit_button.grid(row=5, columnspan=2)
-
-		# Add padding to all widgets
-		for child in self.content.winfo_children(): 
-				child.grid_configure(padx=5, pady=5)
-
-
-	def calculate(self, *args):
-		try:
-			for stat, value in self.stats.items():
-				print(f'{stat}: {value.get()}')
-		except ValueError:
-			pass
-
-
-if __name__ == '__main__':
-	app = App()
-	app.mainloop()
+@app.route('/')
+def index():
+	result = compute_avg_dps(1000, 100, 150, 84, 900, 0, True, True, True)
+	return f'Simulated 1000 fights, average dps: {result}'
---
+++ archive/gui.py
@@ -0,0 +1,53 @@
+from tkinter import *
+from tkinter import ttk
+
+class App(Tk):
+	def __init__(self):
+		super().__init__()
+		self.title('Moonkin DPS Simulator')
+		
+		# Init the main content frame 
+		self.content = ttk.Frame(self, padding="3 3 12 12")
+		self.content.grid(column=0, row=0, sticky=(N, W, E, S))
+
+		# Setup character stat variables for entry widgets
+		self.stats = {
+			'Intellect'   			: StringVar(name='int',        value=115),
+			'Spellpower'  			: StringVar(name='sp',         value=0),
+			'Spell Hit Rating'  	: StringVar(name='spell_hit',  value=0),
+			'Spell Crit Rating'		: StringVar(name='spell_crit', value=0),
+			'Spell Haste' 			: StringVar(name='haste', 	   value=0)
+		}
+		
+		# Build stat input widgets & labels
+		self.create_widgets()
+
+	def create_widgets(self):
+		# Create a row w/ an entry and label for each player stat
+		for idx, (stat_name, tkVar) in enumerate(self.stats.items()):
+			entry = ttk.Entry(self.content, width=7, textvariable=tkVar)
+			label = ttk.Label(self.content, text=stat_name)
+
+			entry.grid(column=0, row=idx)
+			label.grid(column=1, row=idx)
+
+		# Row 5 / Calculate Button
+		submit_button = ttk.Button(self.content, command=self.calculate, text='Calculate DPS')
+		submit_button.grid(row=5, columnspan=2)
+
+		# Add padding to all widgets
+		for child in self.content.winfo_children(): 
+				child.grid_configure(padx=5, pady=5)
+
+
+	def calculate(self, *args):
+		try:
+			for stat, value in self.stats.items():
+				print(f'{stat}: {value.get()}')
+		except ValueError:
+			pass
+
+
+if __name__ == '__main__':
+	app = App()
+	app.mainloop()
