1f81adc8e29a553c784ad8767dbdb5ba57d2764d
Caleb Fringer <cfringer98@gmail.com>
2021-07-01 12:45:21 -0700

    Created stat input page (form.html) and configured Flask to serve it from index route

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from simulation import compute_avg_dps
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_folder='public')
 
 @app.route('/')
 def index():
-	result = compute_avg_dps(1000, 100, 150, 84, 900, 0, True, True, True)
-	return f'Simulated 1000 fights, average dps: {result}'
+	#result = compute_avg_dps(1000, 100, 150, 84, 900, 0, True, True, True)
+	#return f'Simulated 1000 fights, average dps: {result}'
+	return app.send_static_file('form.html')
