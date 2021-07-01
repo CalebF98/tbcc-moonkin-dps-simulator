c34b2a06d7e970d8eb17edd5a806173d5e799550
Caleb Fringer <cfringer98@gmail.com>
2021-07-01 13:58:52 -0700

    Implemented '/' POST route, right now it simply returns the values submitted from the form to the browser.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from simulation import compute_avg_dps
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__, static_folder='public')
 
-@app.route('/')
+@app.get('/')
 def index():
 	#result = compute_avg_dps(1000, 100, 150, 84, 900, 0, True, True, True)
 	#return f'Simulated 1000 fights, average dps: {result}'
 	return app.send_static_file('form.html')
+
+@app.post('/')
+def simulate():
+	return request.form
