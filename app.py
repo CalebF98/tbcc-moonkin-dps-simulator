af0b88cc292479432d7a764920ca30b0281f0b38
Caleb Fringer <cfringer98@gmail.com>
2021-07-02 13:50:01 -0700

    Set up wtforms to validate & render the input form

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+import os
+from dotenv import load_dotenv
 from simulation import compute_avg_dps
-from flask import Flask, request
-app = Flask(__name__, static_folder='public')
+from flask import Flask, request, render_template
+from form import SimParamsForm
 
-@app.get('/')
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(32)
+
+
+@app.route('/', methods=['GET','POST'])
 def index():
-	#result = compute_avg_dps(1000, 100, 150, 84, 900, 0, True, True, True)
-	#return f'Simulated 1000 fights, average dps: {result}'
-	return app.send_static_file('form.html')
+	form = SimParamsForm()
+	if request.method == 'POST' and form.validate():
+		return render_template('form.html', form=form, results='Congratulations! You entered valid data.')
 
-@app.post('/')
-def simulate():
-	return request.form
+	return render_template('form.html', form=form)
---
+++ form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.fields.simple import SubmitField
+from wtforms.validators import DataRequired, NumberRange
+
+class SimParamsForm(FlaskForm):
+	intellect  = IntegerField('Intellect', 					[NumberRange(0,1000)])
+	spellpower = IntegerField('Spellpower',         [NumberRange(0,1000)])
+	hit        = IntegerField('Spell Hit Rating',   [NumberRange(0,202)])
+	crit       = IntegerField('Spell Crit Rating',  [NumberRange(0,500)])
+	haste      = IntegerField('Spell Haste Rating', [NumberRange(0,1000)])
+	num_fights = IntegerField('Number of fights to simulate', [NumberRange(1,2500)])
